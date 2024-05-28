db67fd5e315d3b7a751675caf5b79662de2b6078
agonzalez2022198 <agonzalez-2022198@kinal.edu.gt>
2024-05-28 01:47:42 -0600

    post users

---
+++ src/user/user.controller.js
@@ -0,0 +1,27 @@
+import { response, request } from 'express';
+import bcryptjs from 'bcryptjs';
+import User from './user.model.js';
+
+
+export const postUser = async (req, res) => {
+    const {
+        name, nickName, userName, password, DPI, 
+        location, celular, correo, monthlyIncome
+    } = req.body;
+
+    const user = new User({
+        name, nickName, userName, password, DPI, 
+        location, celular, correo, monthlyIncome
+    });
+
+    const salt = bcryptjs.getSaltSync();
+    usuario.password = bcryptjs.hashSync(password, salt);
+
+    await user.save();
+
+    res.status(200).json({
+        user
+    });
+
+    
+}
---
+++ src/user/user.model.js
@@ -0,0 +1,79 @@
+import mongoose, {Scheema} from 'mongoose';
+
+const  UserSchema = mongoose.Schema({
+    name: {
+        type: String,
+        require: true
+    },
+
+    nickName: {
+        type: String,
+        require: true
+    },
+
+    userName: {
+        type: String,
+        require: true
+    },
+
+    password: {
+        type: String,
+        require: true
+    },
+
+    USER_ROLE: {
+        type: String,
+        default: 'USER_ROLE'
+    },
+
+    DPI: {
+        type: String,
+        require: true
+    },
+
+    location: {
+        type: String,
+        require: true
+    },
+
+    celular: {
+        type: String,
+        require: true
+    },
+
+    correo: {
+        type: String,
+        require: true
+    },
+
+    monthlyIncome: {
+        type: Number,
+        require: true
+    },
+
+    credit: {
+        type: Number
+        //require: true
+    },
+
+    searches: {
+        type: mongoose.Types.ObjectId()
+    },
+
+    state: {
+        type: Boolean,
+        defaut: true
+    }
+
+});
+
+
+UserSchema.methods.toJSON = function(){
+    const { __v, password, _id, ...usuario} = this.toObject();
+    usuario.uid = _id;
+    return usuario;
+}
+
+
+
+export default mongoose.model('User', UserSchema);

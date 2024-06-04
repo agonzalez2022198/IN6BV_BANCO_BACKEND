1d3c8d94d6631ba62b1fb8a335760c7c248156d2
ygarcia2022194 <ygarcia-2022194@kinal.edu.gt>
2024-06-04 16:31:09 -0600

    Se termino el crud completo para account

--- src/account/account.controller.js
+++ src/account/account.controller.js
@@ -16,3 +16,79 @@ export const createAccount = async (req, res) => {
         res.status(400).json({ message: error.message });
     }
 };
+
+export const getAccount = async (req, res) =>{
+    try {
+        const account = await Account.find();
+        res.status(200).json(account);
+      } catch (error) {
+        console.error('Error al obtener las cuentas:', error);
+        res.status(500).json({ mensaje: 'Error interno del servidor.' });
+      }
+}
+
+export const getAccountById = async (req, res) =>{
+    const accountId = req.params.id;
+
+  try {
+    const account = await Account.findById(accountId);
+    if (!account) {
+      return res.status(404).json({ mensaje: 'Cuenta no encontrada.' });
+    }
+    res.status(200).json(account);
+  } catch (error) {
+    console.error('Error al obtener la cuenta:', error);
+    res.status(500).json({ mensaje: 'Error interno del servidor.' });
+  }
+}
+
+export const updateAccount = async (req, res) =>{
+    const accountId = req.params.id;
+  const { accountNumber, user, typeAccount, favoriteAccount } = req.body;
+
+  try {
+    let account = await Account.findById(accountId);
+
+    if (!account){
+        return res.status(404).json({ 
+            mensaje: 'Cuenta no encontrada.' 
+        });
+    }
+
+    account.accountNumber = accountNumber;
+    account.user = user;
+    account.typeAccount = typeAccount;
+    account.favoriteAccount = favoriteAccount;
+    await account.save();
+
+    res.status(200).json({ 
+        mensaje: 'Cuenta actualizada correctamente.' 
+    });
+  } catch (error) {
+    console.error('Error al actualizar la cuenta:', error);
+    res.status(500).json({ mensaje: 'Error interno del servidor.' });
+  }
+}
+
+export const deleteAccount = async (req, res) =>{
+    const accountId = req.params.id;
+
+  try {
+    const cuentaEliminada = await Account.findByIdAndDelete(accountId);
+
+    if (!cuentaEliminada) {
+        return res.status(404).json({ 
+            mensaje: 'Cuenta no encontrada.' 
+        });
+    }
+
+    res.status(200).json({ 
+        mensaje: 'Cuenta eliminada correctamente.'
+    });
+  } catch (error) {
+    console.error('Error al eliminar la cuenta:', error);
+    res.status(500).json({ 
+        mensaje: 'Error interno del servidor.' 
+    });
+  }
+}
--- src/account/account.routes.js
+++ src/account/account.routes.js
@@ -1,9 +1,21 @@
 import express from 'express';
-import { createAccount } from './account.controller.js';
+import { createAccount, 
+        deleteAccount, 
+        getAccount, 
+        getAccountById, 
+        updateAccount } from './account.controller.js';
 
 const router = express.Router();
 
 // Definir la ruta POST para crear una nueva cuenta
 router.post('/', createAccount);
+
+router.get('/', getAccount);
+
+router.get('/:id', getAccountById);
+
+router.put('/:id', updateAccount);
+
+router.delete('/:id', deleteAccount);
 
 export default router;
